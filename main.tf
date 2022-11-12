fa8e27e96cc1e205504d3546d4e6fa38abb75d97
hujuu <1961946+hujuu@users.noreply.github.com>
2022-11-12 23:29:40 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
-variable "access_key" {}
-variable "secret_key" {}
-
 provider "aws" {
   region     = "ap-northeast-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 
 terraform {
