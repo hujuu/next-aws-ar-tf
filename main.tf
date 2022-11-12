cea1167b0bc2d8df7ee80e3a0fe91b941a914c58
hujuu <1961946+hujuu@users.noreply.github.com>
2022-11-12 22:49:47 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
- terraform {
+variable "access_key" {}
+variable "secret_key" {}
+
+provider "aws" {
+  region     = "ap-northeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+terraform {
   required_providers {
 	aws = {
 	  version = ">= 3.42.0"
